0c7370643bb6c6b3194eca5754201f508c0509e5
TaplierShiru <45129308+TaplierShiru@users.noreply.github.com>
2020-09-09 22:44:45 +0400

    fix(pose_estimation/model/utils/pafprocess/pafprocess): add path to .so file _pafprocess # 4

--- pose_estimation/model/utils/pafprocess/pafprocess.py
+++ pose_estimation/model/utils/pafprocess/pafprocess.py
@@ -13,7 +13,7 @@ if _swig_python_version_info >= (2, 7, 0):
         pkg = __name__.rpartition('.')[0]
         mname = '.'.join((pkg, PATH_TO_SO_FILE)).lstrip('.')
         print(mname)
-        mname = PATH_TO_SO_FILE.replace('.', '/')
+        mname = PATH_TO_SO_FILE.replace('/', '.')
         try:
             return importlib.import_module(mname)
         except ImportError:
