f6e13b370ea426cda51c0e95777508994f90ee2d
okatanaaa <whitemarsstudios@gmail.com>
2020-09-09 18:33:01 +0400

    new: setup file

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+import setuptools
+
+setup(
+    name='MakiPoseNet',
+    packages=setuptools.find_packages(),
+    version='0.5.0',
+    description='A set of utilities for training pose estimation models',
+    long_description='...',
+    author='Kilbas Igor, Gribanov Danil',
+    author_email='igor.kilbas.ai@gmail.com',
+    url='https://github.com/MakiResearchTeam/MakiPoseNet.git',
+    include_package_data=True,  # This will include all files in MANIFEST.in in the package when installing.
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ], install_requires=[]
+)
