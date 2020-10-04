5ca9912631f47bb37284602300f530e53c24ae5a
okatanaaa <whitemarsstudios@gmail.com>
2020-10-04 18:42:05 +0400

    fix(generators): imports

--- pose_estimation/generators/pose_estimation/__init__.py
+++ pose_estimation/generators/pose_estimation/__init__.py
@@ -1,5 +1,5 @@
-
 from .data_preparation import record_mp_pose_estimation_train_data
 from .tfr_map_methods import LoadDataMethod, NormalizePostMethod, RGB2BGRPostMethod, \
-    RIterator, RandomCropMethod, AugmentationPostMethod, BinaryHeatmapMethod
+    RIterator, RandomCropMethod, AugmentationPostMethod, BinaryHeatmapMethod, \
+    ImageAdjustPostMethod, FlipPostMethod
 from .utils import check_bounds, apply_transformation
