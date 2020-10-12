3084ebdb45e10f53b308f601b74805206ef987c0
okatanaaa <whitemarsstudios@gmail.com>
2020-10-12 21:13:35 +0400

    new(MSETrainer): added support for gradient and weight histogram

--- pose_estimation/model/training_modules/mse_trainer.py
+++ pose_estimation/model/training_modules/mse_trainer.py
@@ -44,29 +44,75 @@ class MSETrainer:
         self._training_heatmap = training_heatmap.get_data_tensor()
         self._paf_scale = 1.0
         self._heatmap_scale = 1.0
+        self._gradients = None
         self._loss_is_built = False
         self._optimizer = None
         self._sess = model.get_session()
         assert self._sess is not None
+
+        self._setup_tensorboard_vars()
+
+    def _setup_tensorboard_vars(self):
+        self._tb_is_setup = False
         self._tb_writer = None
         # Counter for total number of training iterations.
         self._tb_counter = 0
+        self._tb_summaries = []
+
+        self._model_layers = self._model.get_layers()
+        # Required for plotting histograms
+        self._layer_weights = {}
+        for layer_name in self._model_layers:
+            self._layer_weights[layer_name] = self._model_layers[layer_name].get_params()
+
+        self._layers_weights_histograms = []
+        self._layers_grads_histograms = []
+
+    def add_summary(self, summary):
+        self._tb_summaries.append(summary)
 
     def set_tensorboard_writer(self, writer):
         """
         Creates logging file for the Tensorboard in the given `logdir_path` directory.
         Parameters
         ----------
-        logdir_path : str
+        writer : tf.FileWriter
             Path to the log directory.
         """
         self._tb_writer = writer
+
+    def add_layers_weights_histograms(self, layer_names):
+        # noinspection PyAttributeOutsideInit
+        self._layers_weights_histograms = layer_names
+
+    def add_layers_grads_histograms(self, layer_names):
+        # noinspection PyAttributeOutsideInit
+        self._layers_grads_histograms = layer_names
 
     def close_tensorboard(self):
         """
         Closes the logging writer for the Tensorboard
         """
         self._tb_writer.close()
+
+    # Currently not used
+    def _find_layer_owner_name(self, var):
+        """
+        Searches for a layer-owner name of the tf.Variable `var`.
+        Parameters
+        ----------
+        var : tf.Variable
+            Variable which owner's name to find.
+
+        Returns
+        -------
+        str
+            Name of the layer-owner.
+        """
+        for layer_name in self._layer_weights:
+            if var is self._layer_weights[layer_name]:
+                return layer_name
+        raise ValueError(f'Could not find a layer-owner of the var = {var}')
 
     def set_loss_scales(self, paf_scale, heatmap_scale):
         """
@@ -85,6 +131,20 @@ class MSETrainer:
         self._paf_scale = paf_scale
         self._heatmap_scale = heatmap_scale
 
+    def _minimize_loss(self, optimizer, global_step):
+        if not self._loss_is_built:
+            self._build_loss()
+            self._loss_is_built = True
+            loss_is_built()
+
+        if self._optimizer != optimizer:
+            self._create_train_op(optimizer, global_step)
+
+        if not self._tb_is_setup:
+            self._setup_tensorboard()
+
+        return self._train_op
+
     def _build_loss(self):
         self._paf_loss = 0.0
         for paf in self._paf_tensors:
@@ -100,29 +160,53 @@ class MSETrainer:
 
         # For Tensorboard
         paf_loss_summary = tf.summary.scalar(MSETrainer.PAF_LOSS, self._paf_loss)
+        self.add_summary(paf_loss_summary)
+
         heatmap_loss_summary = tf.summary.scalar(MSETrainer.HEATMAP_LOSS, self._heatmap_loss)
+        self.add_summary(heatmap_loss_summary)
+
         total_loss_summary = tf.summary.scalar(MSETrainer.TOTAL_LOSS, self._total_loss)
-        self._summary = tf.summary.merge([
-            paf_loss_summary,
-            heatmap_loss_summary,
-            total_loss_summary
-        ])
+        self.add_summary(total_loss_summary)
 
-    def _minimize_loss(self, optimizer, global_step):
-        if not self._loss_is_built:
-            self._build_loss()
-            self._loss_is_built = True
-            loss_is_built()
+    def _create_train_op(self, optimizer, global_step):
+        self._optimizer = optimizer
 
-        if self._optimizer != optimizer:
-            self._optimizer = optimizer
-            self._train_op = optimizer.minimize(
-                self._total_loss, var_list=self._model.get_training_vars(), global_step=global_step
-            )
-            self._sess.run(tf.variables_initializer(optimizer.variables()))
-            new_optimizer_used()
+        if self._gradients is None:
+            training_vars = self._model.get_training_vars()
+            self._gradients = optimizer.get_gradients(training_vars)
+            # Collect mapping from the variable to its grad for tensorboard
+            self._var2grad = dict(zip(training_vars, self._gradients))
 
-        return self._train_op
+        self._train_op = optimizer.minimize(
+            self._total_loss, var_list=self._model.get_training_vars(),
+            global_step=global_step, grad_loss=self._gradients
+        )
+
+        self._sess.run(tf.variables_initializer(optimizer.variables()))
+        new_optimizer_used()
+
+    def _setup_tensorboard(self):
+        assert len(self._tb_summaries) != 0, 'No summaries found.'
+        self._total_summary = tf.summary.merge(self._tb_summaries)
+
+        # Collect all weights histograms
+        for layer_name in self._layers_weights_histograms:
+            with tf.name_scope(f'weight/{layer_name}'):
+                for weight in self._model.get_layer(layer_name):
+                    self.add_summary(tf.summary.histogram(name=weight.name, values=weight))
+
+        # Collect all grads histograms
+        for layer_name in self._layers_weights_histograms:
+            with tf.name_scope(f'grad/{layer_name}'):
+                for weight in self._model.get_layer(layer_name):
+                    grad = self._var2grad.get(weight)
+                    if grad is None:
+                        print(f'Did not find gradient for layer={layer_name}, var={weight.name}')
+                        continue
+                    self.add_summary(tf.summary.histogram(name=weight.name, values=weight))
+
+        self._total_summary = tf.summary.merge(self._tb_summaries)
+        self._tb_is_setup = True
 
     def fit(self, optimizer, epochs=1, iter=10, print_period=None, global_step=None):
         """
