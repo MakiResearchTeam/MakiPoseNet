aa02556ae01a4d00c62c9961e7cb078bcd6717d1
TaplierShiru <45129308+TaplierShiru@users.noreply.github.com>
2020-10-16 11:27:00 +0400

    fix(pose_estimation/metrics/COCO_WholeBody/utils/relayout_coco_annotation): area from keypoints #2

--- pose_estimation/metrics/COCO_WholeBody/utils/relayout_coco_annotation.py
+++ pose_estimation/metrics/COCO_WholeBody/utils/relayout_coco_annotation.py
@@ -107,8 +107,11 @@ def relayout_keypoints(
             y = np.array(new_keypoints[1::3])
             y = y[v > EPS]
 
-            x0, x1, y0, y1 = np.min(x), np.max(x), np.min(y), np.max(y)
-            new_area = (x1 - x0) * (y1 - y0)
+            if len(y) == 0 or len(x) == 0:
+                new_area = 0
+            else:
+                x0, x1, y0, y1 = np.min(x), np.max(x), np.min(y), np.max(y)
+                new_area = (x1 - x0) * (y1 - y0)
         else:
             raise TypeError(f'Unknown mode area type {mode_area_calculation}')
 
