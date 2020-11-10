dd16a4285dd3ee5fd36e49e2df816d8d18f29970
TaplierShiru <45129308+TaplierShiru@users.noreply.github.com>
2020-11-10 10:15:48 +0400

    add(pose_estimation/generators/pose_estimation/__init__): import new methods

--- pose_estimation/generators/pose_estimation/__init__.py
+++ pose_estimation/generators/pose_estimation/__init__.py
@@ -1,5 +1,5 @@
 from .data_preparation import record_mp_pose_estimation_train_data
-from .tfr_map_methods import LoadDataMethod, NormalizePostMethod, RGB2BGRPostMethod, \
-    RIterator, RandomCropMethod, AugmentationPostMethod, BinaryHeatmapMethod, \
-    ImageAdjustPostMethod, FlipPostMethod
+from .tfr_map_methods import (LoadDataMethod, NormalizePostMethod, RGB2BGRPostMethod,
+    RIterator, RandomCropMethod, AugmentationPostMethod, BinaryHeatmapMethod,
+    ImageAdjustPostMethod, FlipPostMethod, ResizePostMethod, ApplyMaskToImagePostMethod)
 from .utils import check_bounds, apply_transformation
