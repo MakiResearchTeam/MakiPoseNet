36f7f78cada5ca7ac27a1593d0faeef13078cba0
okatanaaa <whitemarsstudios@gmail.com>
2020-11-21 20:46:16 +0400

    new(paf_layer_v2): new paf layer

--- pose_estimation/model/__init__.py
+++ pose_estimation/model/__init__.py
@@ -1,3 +1,3 @@
 from .pe_model import PEModel
-from .training_modules import MSETrainer, BinaryHeatmapLayer, PAFLayer
+from .training_modules import MSETrainer, BinaryHeatmapLayer, PAFLayer, PAFLayerV2, GaussHeatmapLayer
 from .core import PETrainer
