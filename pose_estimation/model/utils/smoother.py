3328bcb74ce39107dc1d0515e2956ec5b3c71d9d
TaplierShiru <45129308+TaplierShiru@users.noreply.github.com>
2020-11-26 16:59:03 +0400

    add license

--- pose_estimation/model/utils/smoother.py
+++ pose_estimation/model/utils/smoother.py
@@ -1,5 +1,20 @@
-# vim: sta:et:sw=2:ts=2:sts=2
-# Written by Antonio Loquercio
+# Copyright (C) 2020  Igor Kilbas, Danil Gribanov
+#
+# This file is part of MakiPoseNet.
+#
+# MakiPoseNet is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# MakiPoseNet is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+
 
 import numpy as np
 import scipy.stats as st
