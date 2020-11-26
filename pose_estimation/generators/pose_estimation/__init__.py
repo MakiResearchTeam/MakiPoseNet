423bfdc8b5304e8d91d113d6253132b1113dea42
TaplierShiru <45129308+TaplierShiru@users.noreply.github.com>
2020-11-26 17:02:46 +0400

    add license

--- pose_estimation/generators/pose_estimation/__init__.py
+++ pose_estimation/generators/pose_estimation/__init__.py
@@ -1,3 +1,21 @@
+# Copyright (C) 2020  Igor Kilbas, Danil Gribanov
+#
+# This file is part of MakiPoseNet.
+#
+# MakiPoseNet is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# MakiPoseNet is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+
+
 from .data_preparation import record_mp_pose_estimation_train_data
 from .tfr_map_methods import LoadDataMethod, NormalizePostMethod, RGB2BGRPostMethod, \
     RIterator, RandomCropMethod, AugmentationPostMethod, BinaryHeatmapMethod, \
