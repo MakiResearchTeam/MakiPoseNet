d183e3b0ea09148a049c14dabb4c9f8db4903e19
TaplierShiru <45129308+TaplierShiru@users.noreply.github.com>
2020-12-06 22:49:52 +0400

    fix(pose_estimation/model/pe_model.py): move creation of arrays into stack numpy

--- pose_estimation/model/pe_model.py
+++ pose_estimation/model/pe_model.py
@@ -201,7 +201,7 @@ class PEModel(PoseEstimatorInterface):
             feed_dict={self._input_data_tensors[0]: x}
         )
 
-        # Process PAF
+        # Process paf
         # [N, W, H, NUM_PAFS, 2]
         shape_paf = paf_prediction.shape
         N, num_pafs = shape_paf[0], shape_paf[3]
@@ -209,27 +209,31 @@ class PEModel(PoseEstimatorInterface):
         # [N, W, H, NUM_PAFS * 2] --> [N, NEW_W, NEW_H, NUM_PAFS * 2]
         paf_prediction_reshaped = paf_prediction.reshape(*shape_paf[:-2], -1)
         batched_paf = np.empty((N, resize_to[0], resize_to[1], paf_prediction_reshaped.shape[-1]), dtype=np.float32)
-        final_paf = []
+        _ = np.stack([
+                cv2.resize(
+                    paf_prediction_reshaped[i],
+                    (resize_to[1], resize_to[0]),
+                    interpolation=cv2.INTER_AREA
+                )
+                for i in range(len(paf_prediction_reshaped))
+            ],
+            axis=0,
+            out=batched_paf
+        )
 
-        for i in range(len(paf_prediction_reshaped)):
-            final_paf.append(cv2.resize(
-                paf_prediction_reshaped[i],
-                (resize_to[1], resize_to[0]),
-                interpolation=cv2.INTER_AREA
-            ))
-        _ = np.stack(final_paf, axis=0, out=batched_paf)
-
+        # Process heatmap
         batched_heatmap = np.empty((N, resize_to[0], resize_to[1], heatmap_prediction.shape[-1]), dtype=np.float32)
-
-        final_heatmap = []
-        for i in range(len(heatmap_prediction)):
-            final_heatmap.append(cv2.resize(
-                heatmap_prediction[i],
-                (resize_to[1], resize_to[0]),
-                interpolation=cv2.INTER_AREA
-            ))
-
-        _ = np.stack(final_heatmap, axis=0, out=batched_heatmap)
+        _ = np.stack([
+                cv2.resize(
+                    heatmap_prediction[i],
+                    (resize_to[1], resize_to[0]),
+                    interpolation=cv2.INTER_AREA
+                )
+                for i in range(len(heatmap_prediction))
+            ],
+            axis=0,
+            out=batched_heatmap
+        )
 
         # Get peaks
         batched_heatmap, batched_peaks = self._session.run(
